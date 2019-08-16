d03fc56ee3f89d5e01ff4f95624a75020895947e
Vinicius Batista <viniciusbfs2012@gmail.com>
2019-08-16 10:30:06 -0300

    improve pwa support

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    name: 'FodinhaApp',
+    themeColor: '#F3E5F5',
+    msTileColor: '#F3E5F5',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
+  productionSourceMap: false,
+}
